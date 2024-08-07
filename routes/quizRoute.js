f0fe82af604a00f839ef9bf586f9416d7fd81879
Melisa <melamaachia@gmail.com>
2024-08-07 11:58:43 +0000

    rotes correction

--- routes/quizRoute.js
+++ routes/quizRoute.js
@@ -4,8 +4,8 @@ import { isAuthenticated } from '../middlewares/auth.js';
 
 export const quizRouter = Router();
 
-quizRouter.post('/quiz', isAuthenticated, addQuiz);
-quizRouter.get('/quiz',isAuthenticated, allQuizzes);
-quizRouter.get('quiz/:id', isAuthenticated, getQuiz);
-quizRouter.delete('quiz/:id', isAuthenticated, deleteQuiz);
+quizRouter.post('/quiz/', isAuthenticated, addQuiz);
+quizRouter.get('/quiz/',isAuthenticated, allQuizzes);
+quizRouter.get('/quiz/:id', isAuthenticated, getQuiz);
+quizRouter.delete('/quiz/:id', isAuthenticated, deleteQuiz);
 
--- routes/resetPassword.js
+++ routes/resetPassword.js
@@ -2,8 +2,8 @@ import { Router } from "express";
 import { forgotPassword, resetPassword, verifyCode } from "../controllers/resetPasswordcontroller.js";
 const passwordRouter = Router();
 
-passwordRouter.post("/auth/password/recover", forgotPassword);
-passwordRouter.post("/auth/password/verify-code", verifyCode);
-passwordRouter.post("/auth/password/change", resetPassword);
+passwordRouter.post('/auth/password/recover', forgotPassword);
+passwordRouter.post('/auth/password/verify-code', verifyCode);
+passwordRouter.post('/auth/password/change', resetPassword);
 
 export { passwordRouter };
