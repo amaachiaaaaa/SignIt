dccda8497ce2d49dbe335bbce5029c19a5e990ed
Melisa <melamaachia@gmail.com>
2024-08-19 20:39:48 +0000

    courses

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -18,25 +18,3 @@ export const isAuthenticated = (req, res, next) => {
         return res.status(401).json({error:'Not authenticated'})
     }
 }
-
-
-export const hasPermission = (permission) => {
-    return async (req, res, next) => {
-        try {
-            // Get user id from session or request
-            const id = req.session?.user?.id || req?.user?.id;
-            // Find user by id
-            const user = await userModel.findById(id);
-            // Find user role with permissions
-            const userRole = roles.find(element => element.role === user.role);
-            // Use role to check if user has permission
-            if (userRole && userRole.permissions.includes(permission)) {
-                next();
-            } else {
-                res.status(403).json('Not Authorized!');
-            }
-        } catch (error) {
-            next(error);
-        }
-    }
-}
