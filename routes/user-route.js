6b37b7c43c679acfdc3bfc17be56cba0545cc504
Melisa <melamaachia@gmail.com>
2024-08-19 18:48:44 +0000

    token

--- routes/user-route.js
+++ routes/user-route.js
@@ -8,6 +8,6 @@ userRouter.get('/users/auth/Users', getUsers)
 userRouter.post('/users/auth/signup',signup);
 userRouter.get('/users/auth/:userName', getUser),
 
-userRouter.post('/users/auth/login/token',login)
-userRouter.post('/users/auth/logout', logout),
-userRouter.post('/users/auth/token',token);
+userRouter.post('/users/auth/login/token',token) 
+userRouter.post('/users/auth/logout', logout);
+
